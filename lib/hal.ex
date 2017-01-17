8f41493c7d34917106e9162459d83b273dd085ac
w1gz <w1gz@noreply.com>
2017-01-17 13:13:24 +0100

    Convert the custom configuration to a proper yaml file

--- lib/hal.ex
+++ lib/hal.ex
@@ -12,7 +12,7 @@ defmodule Hal do
     - `client` store the ExIrc client state
     - `host` irc host (chat.freenode.net)
     - `port`  irc port (6667)
-    - `chans` irc channels (["#awesome-chan", "pulp-fiction"]
+    - `chans` irc channels (["#awesome-chan", "#pulp-fiction"]
     - `nick` login for the irc server
     - `pass` the associated password
     - `user` misc infos
@@ -21,67 +21,57 @@ defmodule Hal do
     """
 
     defstruct client: nil,
-      host: nil,
-      port: nil,
-      chans: nil,
-      nick: nil,
-      pass: nil,
-      user: nil,
-      name: nil,
-      uids: nil
+      host: "127.0.0.1",
+      port: 6667,
+      chans: ["#hal", "#test"],
+      nick: "hal",
+      user: "hal",
+      name: "hal",
+      pass: "",
+      uids: %{}
   end
 
   def start(_type, [credentials]) do
     import Supervisor.Spec, warn: false
 
-    # parse connection/server infos
-    data = case File.read("apps/hal9k/" <> credentials) do
-             {:ok, data} -> data
-             {:error, _reason} ->
-               # provides a "dummy" configuration
-               "[local]\n" <>
-                 "nick: hal\n" <>
-                 "user: hal\n" <>
-                 "name: hal\n" <>
-                 "host: 127.0.0.1\n" <>
-                 "port: 6667\n" <>
-                 "pass: \n" <>
-                 "chans: #yolo, #too, #hal\n"
-           end
-    match = Regex.named_captures(~r/\[.*\]\n(?<server>.*)\n+\[?/muis, data)
+    # try to read config file
+    confs = try do
+              YamlElixir.read_all_from_file(credentials)
+            rescue
+              _ -> [Map.new()]
+            else
+              [yaml] -> import_conf(yaml["servers"])
+            end
 
-    # format for our internal struture
-    conf = format_internal_state(match)
-    IO.puts("[INFO] credentials were successfully read")
-
-    # start everything up
-    {:ok, client} = ExIrc.start_client!
-    args = %State{client: client} |> Map.merge(conf)
-    children = [
+    global = [
       worker(Hal.Keeper, [[], [name: :hal_keeper]]),
       worker(Hal.Shepherd, [[], [name: :hal_shepherd]]),
-      supervisor(Hal.IrcSupervisor, [args, [name: :hal_irc_supervisor]]),
-      supervisor(Hal.PluginSupervisor, [args, [name: :hal_plugin_supervisor]])
     ]
+
+    plugins_and_handlers = Enum.map(confs, fn(args) ->
+      [
+        supervisor(Hal.PluginSupervisor, [args, []]),
+        supervisor(Hal.IrcSupervisor, [args, []])
+      ]
+    end)
+
+    # TODO accept multiple servers instead of the first one. This will be
+    # implemented after we transition to Mnesia
+    children = global ++ hd(plugins_and_handlers)
     Supervisor.start_link(children, strategy: :one_for_one)
   end
 
-  defp format_internal_state(match) do
-    match["server"]
-    |> String.split("\n")
-    |> Enum.map(fn(line) ->
-      [name | value] = String.split(line, ": ")
-      case name do
-        "port" ->
-          {String.to_atom(name), String.to_integer(hd(value))}
-        "chans" ->
-          value = String.split(hd(value), ", ")
-          {String.to_atom(name), value}
-        _ ->
-          {String.to_atom(name), hd(value)}
-      end
+  defp import_conf(servers) do
+    Enum.map(servers, fn(s) ->
+      {:ok, client} = ExIrc.start_client!
+      %State{client: client,
+             port: s["port"],
+             chans: s["chans"],
+             nick: s["nick"],
+             user: s["user"],
+             name: s["name"],
+             pass: s["pass"]}
     end)
-    |> Map.new(&(&1))
   end
 
 end
